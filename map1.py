e43aeaeb59877faa1070a3e939e3351a749852f9
FilipRegenczuk <f.regenczuk@outlook.com>
2019-09-20 11:08:31 +0200

    adding layer control panel

--- map1.py
+++ map1.py
@@ -34,19 +34,21 @@ Continent: %s
 """
 
 
-fg = folium.FeatureGroup(name="My map")
+fgp = folium.FeatureGroup(name="Populations")
 
-fg.add_child(folium.GeoJson(data=open("population.json", 'r', encoding='utf-8-sig').read(),
+fgp.add_child(folium.GeoJson(data=open("population.json", 'r', encoding='utf-8-sig').read(),
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']<5000000
 else 'yellow' if 5000000 <= x['properties']['POP2005'] < 20000000
 else 'orange' if 20000000 <= x['properties']['POP2005'] < 50000000
 else 'lightred' if 50000000 <= x['properties']['POP2005'] < 100000000
 else 'red'}))
 
+fgc = folium.FeatureGroup(name="Capitals")
 for lat, lon, name, ctr, con in zip(capit_lat, capit_lon, capit_name, country_name, continent_name):
     frame = folium.IFrame(html=html % (name,ctr,con), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location=[lat, lon], radius=5, popup=folium.Popup(frame), fill_color=set_color(con), color='grey', fill_opacity=0.9))
+    fgc.add_child(folium.CircleMarker(location=[lat, lon], radius=5, popup=folium.Popup(frame), fill_color=set_color(con), color='grey', fill_opacity=0.9))
 
-
-map.add_child(fg)
+map.add_child(fgp)
+map.add_child(fgc)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
